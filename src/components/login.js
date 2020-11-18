9841aa12606b1d93c8979046ef77a325189f0eae
Leonard Tan <tetratorus@gmail.com>
2020-11-18 13:33:25 +0800

    Update package

--- src/components/login.js
+++ src/components/login.js
@@ -3,9 +3,8 @@ import DirectWebSdk from '@toruslabs/torus-direct-web-sdk'
 
 const torus = new DirectWebSdk({
   baseUrl: 'http://localhost:8000/serviceworker/',
-  GOOGLE_CLIENT_ID: process.env.GATSBY_GOOGLE_CLIENT_ID,
-  proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
-  network: 'ropsten', // details for test net,
+  // proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
+  // network: 'ropsten', // details for test net,
   enableLogging: true
 })
 
@@ -18,10 +17,11 @@ async function login () {
 
   await initTorus()
 
-  const userInfo = await torus.triggerLogin(
-    'google',
-    process.env.GATSBY_VERIFIER_NAME
-  )
+  const userInfo = await torus.triggerLogin({
+    verifier: 'mubc-google',
+    typeOfLogin: 'google',
+    clientId: '1062557508086-44j40vu7g0dg34pi32ae6kq3arjm6o1j.apps.googleusercontent.com'
+  })
 
   console.log(`userInfo : ${JSON.stringify(userInfo, null, 2)}`)
 }
